461c2f7cd25ead57edcf03b0a64647b3dd14a8d3
vinitmodi.dev <vinitmodi.dev@gmail.com>
2020-06-10 01:09:19 +0530

    form added

--- generator/views.py
+++ generator/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request,'generator/home.html')
+    return render(request, 'generator/home.html')
 
 def password(request):
-    return render(request,'generator/password.html')
+    return render(request, 'generator/password.html')
--- password_generator/urls.py
+++ password_generator/urls.py
@@ -20,5 +20,5 @@ from generator import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='Home'),
-    path('password/', views.password, name='password')
+    path('password/', views.password, name='password'),
 ]
